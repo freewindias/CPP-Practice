084ce605dac77936e17c41b84a222f82899c5933
freewindias <freewindias6@gmail.com>
2026-01-31 19:39:01 -0800

    01

---
+++ 01-HelloWorld/01-HelloWorld/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  01-HelloWorld
+//
+//  Created by Freewin Dias on 31/01/26.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return EXIT_SUCCESS;
+}
