0a26cbecea87de6f5ae73733a5efdd629ae83c0c
freewindias <freewindias6@gmail.com>
2026-01-31 19:33:43 -0800

    Initial Commit

---
+++ Practice/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Practice
+//
+//  Created by Freewin Dias on 31/01/26.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return EXIT_SUCCESS;
+}
