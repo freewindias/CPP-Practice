e6bc6e8bf5021a6f6402031db3a653ba533a313a
freewindias <freewindias6@gmail.com>
2026-01-31 19:40:19 -0800

    02

--- 02-Inputs/02-Inputs/main.cpp
+++ 02-Inputs/02-Inputs/main.cpp
@@ -1,6 +1,6 @@
 //
 //  main.cpp
-//  Practice
+//  02-Inputs
 //
 //  Created by Freewin Dias on 31/01/26.
 //
@@ -10,6 +10,5 @@
 int main(int argc, const char * argv[]) {
     // insert code here...
     std::cout << "Hello, World!\n";
-    std::cout << "MC";
     return EXIT_SUCCESS;
 }
